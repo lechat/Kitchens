475ec48657782535bb4cca4eafdb2b0862ddb9b7
lechat <ctpeko3a-github@gmail.com>
2012-05-06 09:55:54 +0100

    Changed parameters in base kitchen

--- my_kitchen/config/base.py
+++ my_kitchen/config/base.py
@@ -1,15 +1,12 @@
 
 def base(kit):
     kit.add_cookbook_path(
-        '/home/xleap/src/cookbooks', 'kokki.cookbooks'
+        '/home/aleksey/src/cookbooks', 'kokki.cookbooks'
         )
     kit.update_config({
-        'users': ['user_ss1', 'user2'],
-        'file_path': '/tmp/simple_user_file.txt',
-        # 'uname_param': '-s',
-        'weblogic_base_port': 8080,
-        'wl_datasource': { 'ds1': 'MDM', 'ds2': 'XLEAP' },
-        'wl_jms' : {'q1'}
+        'managed_server_1_port': 12010,
+        'managed_server_2_port': 12020,
+        'admin_server_port': 12001,
     })
     kit.include_recipe("stop_server")
 
